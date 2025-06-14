a5ffb264808ade5230c3c20be29e86ac6e65679b
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-06-14 19:54:42 +0200

    perf(problem01): reduce data sizes

--- problem01/src/lib.rs
+++ problem01/src/lib.rs
@@ -68,39 +68,44 @@ fn eni_part2(n: u64, exp: u64, mod_: u64) -> u64 {
         .unwrap()
 }
 
-fn eni_part3(n: u64, exp: u64, mod_: u64) -> u64 {
+fn eni_part3(n: u64, exp: u64, r#mod: u16) -> u64 {
     let mut score = 1;
-    let mut l = Vec::with_capacity(mod_ as usize);
+    let mut l = Vec::with_capacity(r#mod as usize);
     let mut sum = 0;
 
-    let mut last_seen_at = vec![usize::MAX; mod_ as usize];
+    let mut last_seen_at = vec![u16::MAX; r#mod as usize];
     let mut len = 0;
-    let mut skipped = false;
 
     while len < exp as usize {
         score *= n;
-        score %= mod_;
+        score %= r#mod as u64;
         sum += score;
 
         l.push(score);
         len += 1;
 
-        if !skipped && last_seen_at[score as usize] != usize::MAX {
-            let cycle_start = last_seen_at[score as usize];
+        if last_seen_at[score as usize] != u16::MAX {
+            let cycle_start = last_seen_at[score as usize] as usize;
             let cycle_length = l.len() - cycle_start - 1;
 
             l.pop();
-            let looping_bit = &l[cycle_start..];
+            let r#loop = &l[cycle_start..];
+            let loop_sum = sum - score - l[..cycle_start].iter().sum::<u64>();
+
             let remaining = exp as usize - len;
             let full_cycles = remaining / cycle_length;
 
-            len += (full_cycles * cycle_length) as usize;
-            sum += looping_bit.iter().sum::<u64>() * full_cycles as u64;
+            sum += loop_sum * full_cycles as u64;
 
-            skipped = true;
-            continue;
+            sum += r#loop
+                .iter()
+                .skip(1)
+                .take(remaining % cycle_length)
+                .sum::<u64>();
+
+            break;
         }
-        last_seen_at[score as usize] = l.len() - 1;
+        last_seen_at[score as usize] = l.len() as u16 - 1;
     }
 
     sum
@@ -123,7 +128,6 @@ struct Row {
 impl Row {
     fn from_line(line: &str) -> Self {
         let mut parts = line.split_whitespace();
-        let m = parse_kv(parts.next_back().unwrap());
 
         let a = parse_kv(parts.next().unwrap());
         let b = parse_kv(parts.next().unwrap());
@@ -131,6 +135,7 @@ impl Row {
         let x = parse_kv(parts.next().unwrap());
         let y = parse_kv(parts.next().unwrap());
         let z = parse_kv(parts.next().unwrap());
+        let m = parse_kv(parts.next().unwrap());
 
         Row {
             a,
@@ -159,9 +164,9 @@ pub fn solve_part3() -> u64 {
         .par_lines()
         .map(Row::from_line)
         .map(|row| {
-            eni_part3(row.a, row.x, row.m)
-                + eni_part3(row.b, row.y, row.m)
-                + eni_part3(row.c, row.z, row.m)
+            eni_part3(row.a, row.x, row.m as u16)
+                + eni_part3(row.b, row.y, row.m as u16)
+                + eni_part3(row.c, row.z, row.m as u16)
         })
         .max()
         .unwrap();
